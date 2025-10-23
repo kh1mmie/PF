422d897ea5745d17343cfd20df38eaf3bdcc5f91
unknown <khimmie40@gmail.com>
2025-10-24 00:55:26 +0700

    add_Abstract_BG

--- src/components/ui/AbstractBackground.jsx
+++ src/components/ui/AbstractBackground.jsx
@@ -1,42 +1,34 @@
 export default function AbstractBackground() {
-
   return (
-
     <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
-
-         {/* top_R */}
+      {/* bot_L */}
       <div
-        className="absolute w-[40rem] h-[40rem] 
-                   bg-gradient-to-r from-Magenta to-MidnightPurple1 
-                   -top-24 -right-24 
-                   rounded rounded-s-lg filter blur-3xl opacity-40"
-      ></div>
-
-      {/* bot_R */}
-      <div
-        className="absolute w-80 h-80 bg-Lavender bottom-0 -right-48
-                    rounded-full filter blur-3xl opacity-50"
-      ></div>
-
-       {/* middle_center */}
-      <div
-        className="absolute w-[45rem] h-[34rem] bg-gradient-to-tr from-MidnightPurple3 to-MidnightPurple1 
-                   top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-                   rounded-full filter blur-3xl opacity-20"
+        className="absolute w-[30rem] h-[30rem] 
+                    bg-gradient-to-tr from-DeepNavyblue via-BlackBurry to-MidnightPurple1 
+                    -bottom-[11rem] -left-[3rem] 
+                    rounded-tl-[10rem] rounded-tr-[5rem] 
+                    rounded-bl-[12rem] rounded-br-[8rem]
+                    filter blur-3xl opacity-20"
       ></div>
 
       {/* middle_L */}
       <div
-        className="absolute w-32 h-54 bg-Magenta top-48 left-1/4 
-                    rounded-b-md rounded-bl-xs filter blur-3xl opacity-20"
+        className="absolute w-80 h-60
+                    bg-radial-[at_30%_30%] from-Lavender via-bbMilk to-DeepNavyblue 
+                    -top-24 left-1/4
+                    rounded-tl-[40%] rounded-tr-[60%] 
+                    rounded-bl-[70%] rounded-br-[30%] 
+                    filter blur-3xl opacity-20"
       ></div>
 
-           {/* bot_L */}
+      {/* top_R */}
       <div
-        className="absolute w-123 h-123 bg-Green3 -bottom-45 -left-12 
-                    rounded-tr-xs rounded-br-3xl rounded-bl rotate-68 filter blur-3xl opacity-20"
+        className="absolute w-[45rem] h-[60rem] 
+                    bg-gradient-to-tl from-MidnightPurple via-bbMilk to-DeepNavyblue -top-24 -right-48 rotate-x-35 -rotate-y-13
+                    rounded-tl-[90%] rounded-tr-[120%] 
+                    rounded-bl-[30%] rounded-br-[67%]
+                    filter blur-3xl opacity-30"
       ></div>
-      
     </div>
   );
 }
